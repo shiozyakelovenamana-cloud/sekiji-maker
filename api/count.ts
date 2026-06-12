e055d1a7989791b02a9405be39cce0b3d7258b94
shiozyakelovenamana-cloud <shiozyake.love.namana@gmail.com>
2026-06-12 20:24:37 +0900

    Remove edge runtime configuration from count.ts

--- api/count.ts
+++ api/count.ts
@@ -21,4 +21,3 @@ export default async function handler(req: Request): Promise<Response> {
   return new Response('Method Not Allowed', { status: 405 });
 }
 
-export const config = { runtime: 'edge' };
